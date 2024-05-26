761ee43d6a0c5f42d8cb6f97396c6c9c8aa2df4d
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2024-05-26 08:56:39 +0200

    Zad 5

--- Zad_5/zad5.java
+++ Zad_5/zad5.java
@@ -1,12 +1,33 @@
 package Zad_5;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class zad5 {
     public static void main(String[] args) {
-        System.out.println("Stuff for sale:");
+        Scanner input = new Scanner(System.in);
+        System.out.println("How much you want to borrow? Min:100 Max:$10.000,00.");
+        BigDecimal principal = getValidBigDecimalInput(input, "principal");
+
+        System.out.println("For how long? Min:6 m Max:48 months.");
+        BigDecimal numberPeriods = getValidBigDecimalInput(input, "installmentPlan");
+
+        BigDecimal rate = rate(numberPeriods);
+
+        System.out.println("Principal: " + principal + " number of periods " + numberPeriods);
+
+        MathContext mc = new MathContext(4);
+
+        BigDecimal paydOfPrincipal = BigDecimal.valueOf(0.00);
+
+        for (int i = 0; numberPeriods.compareTo(BigDecimal.valueOf(i)) > 0 ; i++){
+            BigDecimal instalment = principal.divide(numberPeriods, mc);
+            System.out.println("This month pricipal to pay: " + instalment);
+            paydOfPrincipal = paydOfPrincipal.add(instalment);
+            System.out.println("Paid of pricipal: " + paydOfPrincipal);
+        }
         
     }
 
@@ -26,16 +47,30 @@ public class zad5 {
         } while (true); // Loop indefinitely until a valid prompt is entered.
     }
     public static boolean validatedInput(BigDecimal input, String typeInput){
-        if (typeInput == "stuff"){
-            if (input.compareTo(BigDecimal.valueOf(100)) == -1 && input.compareTo(BigDecimal.valueOf(10000)) == 1){
+        if (typeInput == "principal"){
+            if (input.compareTo(BigDecimal.valueOf(100)) >= 0 && input.compareTo(BigDecimal.valueOf(10000)) <= 0){
                 return true;
+            } else {
+                System.out.println("Min:100 Max:10.000,00");
             }
         } else if (typeInput == "installmentPlan"){
-            if (input.compareTo(BigDecimal.valueOf(6)) == 1 && input.compareTo(BigDecimal.valueOf(48)) == -1){
+            if (input.compareTo(BigDecimal.valueOf(6)) >= 0 && input.compareTo(BigDecimal.valueOf(48)) <= 0){
                 return true;
+            } else {
+                System.out.println("Min:6 Max:48.");
             }
         }
         return false;
     }
-    
+    public static BigDecimal rate(BigDecimal numberPeriods){
+        BigDecimal firstBucket = BigDecimal.valueOf(12);
+        BigDecimal secondBucket = BigDecimal.valueOf(24);
+
+        if (numberPeriods.compareTo(firstBucket) <= 0){
+            return BigDecimal.valueOf(.025);
+        } else if (numberPeriods.compareTo(secondBucket) <= 0){
+            return BigDecimal.valueOf(.05);
+        } 
+        return BigDecimal.valueOf(.1);
+    }
 }
--- Zad_5/zad5ttest.java
+++ Zad_5/zad5ttest.java
@@ -7,28 +7,47 @@ import org.junit.Test;
 public class zad5ttest {
     @Test
     public void validatedInput() {
+        String inputTypePrincipal= "principal";
+        String inputTypePlan = "installmentPlan";
+        Boolean expectedTrue = true;
+        Boolean expectedFalse = false;
+
         // Test case 1: Stuff
         BigDecimal input1 = BigDecimal.valueOf(50000);
-        String inputType1 = "stuff";
-        Boolean expected1 = false;
-        assertEquals(expected1, zad5.validatedInput(input1, inputType1));
+        assertEquals(expectedFalse, zad5.validatedInput(input1, inputTypePrincipal));
+
+        // Test case 0: Stuff
+        BigDecimal input_zero = BigDecimal.valueOf(99);
+        assertEquals(expectedFalse, zad5.validatedInput(input_zero, inputTypePrincipal));
+
+        // Test case 1.1: principal
+        BigDecimal input_low = BigDecimal.valueOf(111);
+        assertEquals(expectedTrue, zad5.validatedInput(input_low, inputTypePrincipal));
+
         // Test case 1: Stuff
         BigDecimal input2 = BigDecimal.valueOf(500000);
-        String inputType2 = "stuff";
-        Boolean expected2 = false;
-        assertEquals(expected2, zad5.validatedInput(input2, inputType2));
+        assertEquals(expectedFalse, zad5.validatedInput(input2, inputTypePrincipal));
 
         // Test case 1: installmentPlan
         BigDecimal input3 = BigDecimal.valueOf(500000);
-        String inputType3 = "installmentPlan";
-        Boolean expected3 = false;
-        assertEquals(expected3, zad5.validatedInput(input3, inputType3));
+        assertEquals(expectedFalse, zad5.validatedInput(input3, inputTypePlan));
 
         // Test case 5: installmentPlan
         BigDecimal input4 = BigDecimal.valueOf(36);
-        String inputType4 = "installmentPlan";
-        Boolean expected4 = true;
-        assertEquals(expected4, zad5.validatedInput(input4, inputType4));
+        assertEquals(expectedTrue, zad5.validatedInput(input4, inputTypePlan));
     }
-    
+    @Test
+    public void interestRate() {
+        // Test case 1: firs bucked.
+        assertEquals(BigDecimal.valueOf(.025), zad5.rate(BigDecimal.valueOf(11)));
+
+        // Test case 2: firs bucked.
+        assertEquals(BigDecimal.valueOf(.025), zad5.rate(BigDecimal.valueOf(12)));
+
+        // Test case 3: second bucked.
+        assertEquals(BigDecimal.valueOf(.05), zad5.rate(BigDecimal.valueOf(24)));
+
+        // Test case 4: third bucked.
+        assertEquals(BigDecimal.valueOf(.1), zad5.rate(BigDecimal.valueOf(25)));
+    }
 }
